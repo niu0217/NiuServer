72f3793e0f56830c6f36297346db92f07015b09a
niu0217 <1033856427@qq.com>
2024-07-06 11:32:49 +0800

    完成HTTP协议解析

---
+++ ReactorHTTP-CPP/HttpRequest.h
@@ -0,0 +1,87 @@
+/* ************************************************************************
+> File Name:     HttpRequest.h
+> Author:        niu0217
+> Created Time:  Sat 06 Jul 2024 10:35:58 AM CST
+> Description:   
+ ************************************************************************/
+
+#pragma once
+
+#include "Buffer.h"
+#include "HttpResponse.h"
+#include <stdbool.h>
+#include <map>
+using namespace std;
+
+// 当前的解析状态
+enum class PrecessState:char
+{
+  ParseReqLine,
+  ParseReqHeaders,
+  ParseReqBody,
+  ParseReqDone
+};
+
+// 定义http请求结构体
+class HttpRequest
+{
+public:
+  HttpRequest();
+  ~HttpRequest();
+
+  void reset();
+  void addHeader(const string key, const string value);  // 添加请求头
+  string getHeader(const string key);  // 根据key得到请求头的value
+  bool parseRequestLine(Buffer* readBuf);  // 解析请求行
+  bool parseRequestHeader(Buffer* readBuf);  // 解析请求头
+
+  // 解析http请求协议
+  bool parseHttpRequest(Buffer* readBuf,
+                        HttpResponse* response, 
+                        Buffer* sendBuf,
+                        int socket);
+  // 处理http请求协议
+  bool processHttpRequest(HttpResponse* response);
+
+  // 解码字符串
+  string decodeMsg(string from);
+  const string getFileType(const string name);
+  static void sendDir(string dirName, Buffer* sendBuf, int cfd);
+  static void sendFile(string dirName, Buffer* sendBuf, int cfd);
+
+  inline void setMethod(string method)
+  {
+    m_method = method;
+  }
+  inline void setUrl(string url)
+  {
+    m_url = url;
+  }
+  inline void setVersion(string version)
+  {
+    m_version = version;
+  }
+  // 获取处理状态
+  inline PrecessState getState()
+  {
+    return m_curState;
+  }
+  inline void setState(PrecessState state)
+  {
+    m_curState = state;
+  }
+
+private:
+  char* splitRequestLine(const char* start, 
+                         const char* end,
+                         const char* sub, 
+                         function<void(string)> callback);
+  int hexToDec(char c);
+
+private:
+  string m_method;
+  string m_url;
+  string m_version;
+  map<string, string> m_reqHeaders;
+  PrecessState m_curState;
+};
---
+++ ReactorHTTP-CPP/HttpResponse.h
@@ -0,0 +1,60 @@
+/* ************************************************************************
+> File Name:     HttpResponse.h
+> Author:        niu0217
+> Created Time:  Sat 06 Jul 2024 10:37:40 AM CST
+> Description:   
+ ************************************************************************/
+
+#pragma once
+
+#include "Buffer.h"
+#include <map>
+#include <functional>
+using namespace std;
+
+// 定义状态码枚举
+enum class StatusCode
+{
+  Unknown,
+  OK = 200,
+  MovedPermanently = 301,
+  MovedTemporarily = 302,
+  BadRequest = 400,
+  NotFound = 404
+};
+
+// 定义结构体
+class HttpResponse
+{
+public:
+  function<void(const string, struct Buffer*, int)> sendDataFunc;
+
+  HttpResponse();
+  ~HttpResponse();
+
+  void addHeader(const string key, const string value);  // 添加响应头
+  void prepareMsg(Buffer* sendBuf, int socket);  // 组织http响应数据
+  
+  inline void setFileName(string name)
+  {
+      m_fileName = name;
+  }
+  inline void setStatusCode(StatusCode code)
+  {
+      m_statusCode = code;
+  }
+
+private:
+  StatusCode m_statusCode;
+  string m_fileName;
+  map<string, string> m_headers;
+  // 定义状态码和描述的对应关系
+  const map<int, string> m_info = {
+    {200, "OK"},
+    {301, "MovedPermanently"},
+    {302, "MovedTemporarily"},
+    {400, "BadRequest"},
+    {404, "NotFound"},
+  };
+};
+
